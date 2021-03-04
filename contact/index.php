03875acf61884c018a884ff37a16b3d5f6222c1f
safenanwar <80055496+safenanwar@users.noreply.github.com>
2021-03-04 08:08:42 -0800

    Add files via upload

---
+++ contact/index.php
@@ -0,0 +1,19 @@
+<?php
+include 'connection.php';
+?>
+
+<?=template_header('Home')?>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-800" src="image/home.jpg" style="height: 600px;">
+  </div>
+
+
+  
+</div>
+
+<?=template_footer()?>
